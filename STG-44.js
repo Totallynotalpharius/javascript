2d490d7703c4e72345a046bf5672a8ec48e6021a
Bryson Albers <albersbw@school.hopeseattle.org>
2022-01-25 12:34:19 -0800

    new files

---
+++ STG-44.js
@@ -0,0 +1,15 @@
+// const cars = ['VW','Mercades','Nissan', 'Volvo', 'Porche'];
+// console.log('second element', cars[1]);
+
+// for(let i=0; < cars.length; i++) {
+//     console.log(cars[i], i);
+// }
+
+const numbers = [1,2,3,4,5,6,7,8,9,10]
+
+let total = 0
+
+for(let i=0; i < numbers.length; i++){
+    total = total + numbers[i];
+    console.log('inside for loop',total);
+}
---
+++ tuesday.js
@@ -0,0 +1,23 @@
+let variable1 = 10;
+let result = (10+10) < 20;
+
+console.log();('this is variable 1', variable1);
+if(!result){
+    console.log('the number is greater')
+}
+
+let x = 3.14;
+let y = 10;
+let total = x + y;
+console.log('total', total);
+
+if(total > 10) {
+    console.log('greater than 10');
+}
+
+let a = "Ibram";
+let b = 100;
+let fullName = a + b;
+console.log('fullName', fullName);
+
+console.log('is 20 a number', Number.isNaN(total))
